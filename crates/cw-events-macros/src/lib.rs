8bf87fe50c6666f70ff8862816731e33298e28f3
Amit Prasad <mail@amitprasad.dev>
2024-07-20 19:46:32 -0400

    import in macro

--- crates/cw-events-macros/src/lib.rs
+++ crates/cw-events-macros/src/lib.rs
@@ -96,7 +96,7 @@ pub fn event(attr: TokenStream, item: TokenStream) -> TokenStream {
                 #event_type.to_string()
             }
 
-            fn as_event(&self) -> StdResult<#cosmwasm_std_path::Event> {
+            fn as_event(&self) -> #cosmwasm_std_path::StdResult<#cosmwasm_std_path::Event> {
                 let as_json = #cosmwasm_std_path::to_json_string(&self)?;
                 #attrs
                 Ok(Event::new(self.type_name()).add_attribute("_json", as_json).add_attributes(attrs))
